977e0615f42eb1da47b395e92a25fdf68f883615
John Doe <maximik2011@gmail.com>
2022-06-18 21:16:52 +0300

    weather Demo

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { provide, ref } from 'vue';
+import Footer from './components/Footer.vue';
+import Header from './components/Header.vue';
+import Content from './components/Content.vue';
+   const darkTheme = ref(localStorage.darkTheme ? localStorage.darkTheme : "emerald") ;
+     const switchTheme = () => {
+    darkTheme.value = darkTheme.value === "dark" ? "emerald" : "dark";
+    localStorage.darkTheme = darkTheme.value;
+  };
+provide('theme', darkTheme.value);
+provide('switchTheme', switchTheme)
+</script>
+
+<template>
+  <div :data-theme="darkTheme" class="min-h-screen flex flex-col transition-all text-primary-content">
+    <Header>
+
+    </Header>
+
+    <Content>
+
+    </Content>
+
+    <Footer>
+
+    </Footer>
+  </div>
+</template>
---
+++ src/components/Content.vue
@@ -0,0 +1,268 @@
+<script setup>
+import { ref } from "vue";
+// каждый 7 элемент массива новфй день  &units=metric
+const userCity = ref("Ростов-на-Дону");
+const error = ref(null);
+const weatherAPI = ref(null);
+const tempAverage = ref(null);
+const cityName = ref("");
+
+// инфа по след дням
+const week = [
+  "Воскресенье",
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+];
+const firstDay = ref({});
+const secondDay = ref({});
+const thirdDay = ref({});
+const today = ref({});
+const dataFirstDay = ref({});
+const dataSecondDay = ref({});
+const dataThirdDay = ref({});
+const srcFirstDay = ref("");
+const srcSecondDay = ref("");
+const srcThirdDay = ref("");
+
+function getWeatherAPI() {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${userCity.value}&units=metric&appid=86095446ea62c353a352f44e0b12a207`
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      if (result.cod === "404" || result.cod === "400") {
+        error.value = result.message;
+      } else {
+        weatherAPI.value = result;
+        // src.value = `http://openweathermap.org/img/wn/${weatherAPI.value.list[0].weather[0].icon}@2x.png`;
+        tempAverage.value = Math.round(
+          (weatherAPI.value.list[0].main.temp_min +
+            weatherAPI.value.list[0].main.temp_max) /
+            2
+        );
+        today.value = weatherAPI.value.list[0];
+        firstDay.value = weatherAPI.value.list[8];
+        secondDay.value = weatherAPI.value.list[16];
+        thirdDay.value = weatherAPI.value.list[24];
+        let todayDayWeek = new Date(today.value.dt_txt.split(" ")[0]);
+        let firstDayWeek = new Date(firstDay.value.dt_txt.split(" ")[0]);
+        let SecondDayWeek = new Date(secondDay.value.dt_txt.split(" ")[0]);
+        let ThirdDayWeek = new Date(thirdDay.value.dt_txt.split(" ")[0]);
+        today.value = {
+          numberDay: today.value.dt_txt.split("-")[2].split(" ")[0],
+          month: today.value.dt_txt.split("-")[1],
+          dayOfWeek: week[todayDayWeek.getDay()],
+          src: `http://openweathermap.org/img/wn/${today.value.weather[0].icon}@2x.png`,
+        };
+        dataFirstDay.value = {
+          numberDay: firstDay.value.dt_txt.split("-")[2].split(" ")[0],
+          month: firstDay.value.dt_txt.split("-")[1],
+          dayOfWeek: week[firstDayWeek.getDay()],
+          src: `http://openweathermap.org/img/wn/${firstDay.value.weather[0].icon}@2x.png`,
+        };
+        dataSecondDay.value = {
+          numberDay: secondDay.value.dt_txt.split("-")[2].split(" ")[0],
+          month: secondDay.value.dt_txt.split("-")[1],
+          dayOfWeek: week[SecondDayWeek.getDay()],
+          src: `http://openweathermap.org/img/wn/${secondDay.value.weather[0].icon}@2x.png`,
+        };
+        dataThirdDay.value = {
+          numberDay: thirdDay.value.dt_txt.split("-")[2].split(" ")[0],
+          month: thirdDay.value.dt_txt.split("-")[1],
+          dayOfWeek: week[ThirdDayWeek.getDay()],
+          src: `http://openweathermap.org/img/wn/${thirdDay.value.weather[0].icon}@2x.png`,
+        };
+        cityName.value = weatherAPI.value.city.name;
+      }
+    });
+
+  tempAverage.value = null;
+  error.value = null;
+  weatherAPI.value = null;
+  userCity.value = "";
+}
+</script>
+
+<template>
+  <div
+    class="bg-base-100 flex-grow transition-all flex lg:items-center lg:justify-center"
+  >
+    <div
+      class="flex flex-col gap-4 p-4 max-w-[1200px] m-auto items-center lg:flex-row lg:justify-between lg:items-stretch"
+    >
+      <!-- форма запроса погоды -->
+      <div
+        class="self-center lg:self-auto bg-base-200 rounded-md flex flex-col items-center gap-4 shadow-md p-4 w-[340px]"
+      >
+        <h1 class="p-2 text-2xl font-bold">Погода сегодня</h1>
+        <form class="flex" @submit.prevent="getWeatherAPI">
+          <input
+            type="text"
+            placeholder="Введите название города"
+            class="input input-bordered input-primary w-full max-w-xs rounded-r-none"
+            v-model="userCity"
+          />
+          <button
+            class="btn btn-primary border-l-0 rounded-l-none"
+            type="submit"
+            :disabled="userCity.length < 3"
+          >
+            <i class="fa-duotone fa-magnifying-glass"></i>
+          </button>
+        </form>
+        <div class="flex flex-col items-center" v-if="weatherAPI">
+          <h3 class="text-center font-bold text-xl">{{ cityName }}</h3>
+          <h5 class="pb-4">
+            {{ today.numberDay }}.{{ today.month }}, {{ today.dayOfWeek }}
+          </h5>
+          <div
+            class="bg-base-300 rounded-full flex items-center justify-center w-[100px] h-[100px]"
+          >
+            <img class="" :src="today.src" alt="cityName" />
+          </div>
+
+          <p class="py-4 font-bold">
+            {{ Math.round(weatherAPI.list[0].main.temp) }}&deg;C,
+            {{ weatherAPI.list[0].weather[0].description }}
+          </p>
+          <div class="flex items-center justify-between gap-4">
+            <div
+              class="bg-base-300 p-2 rounded-md flex flex-col items-center gap-2 w-[90px]"
+            >
+              <i class="fa-duotone fa-droplet-percent text-4xl"></i>
+              <p>{{ weatherAPI.list[0].main.humidity }}%</p>
+            </div>
+            <div
+              class="bg-base-300 p-2 rounded-md flex flex-col items-center gap-2 w-[90px]"
+            >
+              <i class="fa-duotone fa-wind text-4xl"></i>
+              <p>{{ weatherAPI.list[0].wind.speed }} m/s</p>
+            </div>
+            <div
+              class="bg-base-300 p-2 rounded-md flex flex-col items-center gap-2 w-[90px]"
+            >
+              <i class="fa-duotone fa-temperature-half text-4xl"></i>
+              <p>{{ tempAverage }}&deg;C</p>
+            </div>
+          </div>
+        </div>
+        <div v-else-if="error" class="w-[276px]">
+          <h2 class="bg-error text-sm text-slate-200 p-2 rounded-lg">
+            {{ error }}
+          </h2>
+        </div>
+      </div>
+      <!-- прогноз на 3 дня -->
+      <div
+        class="self-center lg:self-auto bg-base-200 rounded-md flex flex-col gap-4 shadow-md p-4 w-[340px]"
+      >
+        <h1 class="text-center lg:p-2 text-2xl font-bold">
+          Погода на 3 дня, <br />
+          {{ cityName }}
+        </h1>
+        <div class="flex flex-col gap-6 items-center" v-if="weatherAPI">
+          <div class="flex flex-col gap-2 items-center">
+            <h2 class="flex items-center">
+              {{ dataFirstDay.numberDay }}.{{ dataFirstDay.month }},
+              {{ dataFirstDay.dayOfWeek }} <span class="px-1">-</span>
+              <span class="font-bold"
+                >{{ Math.round(firstDay.main.temp) }}&deg;C</span
+              >
+              <img :src="dataThirdDay.src" alt="cityName" class="w-12 h-12" />
+            </h2>
+            <div class="flex gap-4">
+              <div
+                class="bg-base-300 p-2 rounded-md flex flex-col items-center gap-2 w-[90px]"
+              >
+                <i class="fa-duotone fa-droplet-percent text-4xl"></i>
+                <p>{{ firstDay.main.humidity }}%</p>
+              </div>
+              <div
+                class="bg-base-300 p-2 rounded-md flex flex-col items-center gap-2 w-[90px]"
+              >
+                <i class="fa-duotone fa-wind text-4xl"></i>
+                <p>{{ firstDay.wind.speed }} m/s</p>
+              </div>
+              <div
+                class="bg-base-300 p-2 rounded-md flex flex-col items-center gap-2 w-[90px]"
+              >
+                <i class="fa-duotone fa-temperature-half text-4xl"></i>
+                <p>{{ tempAverage }}&deg;C</p>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col gap-2 items-center">
+            <h2 class="flex items-center">
+              {{ dataSecondDay.numberDay }}.{{ dataSecondDay.month }},
+              {{ dataSecondDay.dayOfWeek }} <span class="px-1">-</span>
+              <span class="font-bold">
+                {{ Math.round(secondDay.main.temp) }}&deg;C</span
+              >
+              <img :src="dataThirdDay.src" alt="cityName" class="w-12 h-12" />
+            </h2>
+            <div class="flex gap-4">
+              <div
+                class="bg-base-300 p-2 rounded-md flex flex-col items-center gap-2 w-[90px]"
+              >
+                <i class="fa-duotone fa-droplet-percent text-4xl"></i>
+                <p>{{ firstDay.main.humidity }}%</p>
+              </div>
+              <div
+                class="bg-base-300 p-2 rounded-md flex flex-col items-center gap-2 w-[90px]"
+              >
+                <i class="fa-duotone fa-wind text-4xl"></i>
+                <p>{{ firstDay.wind.speed }} m/s</p>
+              </div>
+              <div
+                class="bg-base-300 p-2 rounded-md flex flex-col items-center gap-2 w-[90px]"
+              >
+                <i class="fa-duotone fa-temperature-half text-4xl"></i>
+                <p>{{ tempAverage }}&deg;C</p>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col gap-2 items-center">
+            <h2 class="flex items-center">
+              {{ dataThirdDay.numberDay }}.{{ dataThirdDay.month }},
+              {{ dataThirdDay.dayOfWeek }} <span class="px-1">-</span>
+              <span class="font-bold">
+                {{ Math.round(thirdDay.main.temp) }}&deg;C
+              </span>
+              <img :src="dataThirdDay.src" alt="cityName" class="w-12 h-12" />
+            </h2>
+            <div class="flex gap-4">
+              <div
+                class="bg-base-300 p-2 rounded-md flex flex-col items-center gap-2 w-[90px]"
+              >
+                <i class="fa-duotone fa-droplet-percent text-4xl"></i>
+                <p>{{ firstDay.main.humidity }}%</p>
+              </div>
+              <div
+                class="bg-base-300 p-2 rounded-md flex flex-col items-center gap-2 w-[90px]"
+              >
+                <i class="fa-duotone fa-wind text-4xl"></i>
+                <p>{{ firstDay.wind.speed }} m/s</p>
+              </div>
+              <div
+                class="bg-base-300 p-2 rounded-md flex flex-col items-center gap-2 w-[90px]"
+              >
+                <i class="fa-duotone fa-temperature-half text-4xl"></i>
+                <p>{{ tempAverage }}&deg;C</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- График -->
+      <div
+        class="self-center lg:self-auto bg-base-200 rounded-md flex flex-col items-center gap-4 shadow-md p-4 w-[340px]"
+      >
+        <h1 class="p-2">Grafik</h1>
+      </div>
+    </div>
+  </div>
+</template>
